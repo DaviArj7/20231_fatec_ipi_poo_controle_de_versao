33ad39536f5485d9c912584f08d02f38e7ced8df
Davi Araújo <daviaraujo3078@gmail.com>
2023-05-29 21:50:33 -0300

    Controle de versao

---
+++ Calculadora.java
@@ -0,0 +1,14 @@
+public class Calculadora{
+    public int somar(int n){
+        return (n * (n + 1) / 2);
+     }
+}
+
+/*public class Calculadora{
+    public int soma(int n){
+        for(int i=0; i<=n; i++){
+             resultado += i;
+        }
+    }
+    return resultado;
+} */
